4f7bdd3912edb75df256884d9f1b5adfe83437af
DESKTOP-2ME9CGB\daeho3 <daeho3@naver.com>
2024-10-07 01:28:21 +0900

    [PGS] 문자열 반복해서 출력하기 / lv0

---
+++ 현성/문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = line.split(' ');
+}).on('close', function () {
+    str = input[0];
+    n = Number(input[1]);
+    
+    for(let i = 0; i < n; i++){    
+        process.stdout.write(str);
+    }
+});
