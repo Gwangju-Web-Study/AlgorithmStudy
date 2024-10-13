21648d782a70b1fb5632e1c6ab5ebaa1644e4a08
양민준 <97660495+gwangminjun@users.noreply.github.com>
2024-10-13 14:56:19 +0900

    :sparkles: [PGS] 두 수의 차

---
+++ 민준/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if(num1 == num2){
+            answer = 1;
+        }
+        return answer;
+    }
+}
